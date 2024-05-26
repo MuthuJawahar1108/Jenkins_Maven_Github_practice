cb0583641e626910656fa6efefa88859772558d1
MuthuJawahar1108 <kvmjawa20031108@gmail.com>
2024-05-26 16:07:57 +0530

    First commit

---
+++ myNewProject/src/main/java/com/grp1/myNewProject/App.java
@@ -0,0 +1,13 @@
+package com.grp1.myNewProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Pushing Maven project to Github Demo!" );
+    }
+}
